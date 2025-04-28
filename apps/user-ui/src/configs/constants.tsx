7a5b525e7579e7cbb1d952c524c0fce9434b308e
Shyam Radadiya <shyam.radadiya100@gmail.com>
2025-04-28 15:48:37 +0530

    feat: add user-ui & header component

---
+++ apps/user-ui/src/configs/constants.tsx
@@ -0,0 +1,23 @@
+export const navItems: NavItemsTypes[] = [
+    {
+        title: "Home",
+        href: "/"
+    },
+    {
+        title: "Products",
+        href: "/products"
+    },
+    {
+        title: "Shops",
+        href: "/shops"
+    },
+    {
+        title: "Offers",
+        href: "/offers"
+    },
+    {
+        title: "Become A Seller",
+        href: "/become-seller"
+    },
+
+]
